2aef264fdd6fad9e269410a38ab5f6b49757a3f4
shayed <sashraf171202@bscse.uiu.ac.bd>
2022-01-17 13:06:03 +0600

    chat app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="21981bd3-2541-4cfb-8d37-ddce12b4d95e"
+            userName="shayedashraf"
+            userSecret="123"
+
+        />
+    )
+}
+export default App;
+
+
